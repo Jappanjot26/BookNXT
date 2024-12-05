fbdc2e72b4dfd91d66fc852b9f8cb5b330423d51
Jappanjot26 <singhjappanjot1@gmail.com>
2024-12-05 13:25:17 +0530

    feat: added saved book component

---
+++ src/components/SavedBook.jsx
@@ -0,0 +1,26 @@
+function SavedBook({ book }) {
+  return (
+    <div className="text-white border-b-2  border-translucent m-2 px-4 pt-2 pb-4 flex gap-5">
+      <img src={book.image} alt={book.title} className="h-18 w-12" />
+      <div className="flex flex-col gap-2 w-full">
+        <h1 className="text-base font-semibold">{book.title}</h1>
+        <div className="flex gap-4 w-full items-center">
+          <span className="w-1/3">⭐ 9.20</span>
+          <span className="w-1/3">📄 150</span>
+          <button className="flex w-1/3 justify-end">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 256 256"
+              className="h-5 w-5 bg-red-600 rounded-full p-1 hover:bg-red-700"
+            >
+              <path d="M208.49,191.51a12,12,0,0,1-17,17L128,145,64.49,208.49a12,12,0,0,1-17-17L111,128,47.51,64.49a12,12,0,0,1,17-17L128,111l63.51-63.52a12,12,0,0,1,17,17L145,128Z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SavedBook;
--- src/components/SavedList.jsx
+++ src/components/SavedList.jsx
@@ -25,20 +25,25 @@ const books = [
   },
 ];
 
+import SavedBook from "./SavedBook";
+
 function SavedList() {
   return (
     <>
-      <div className="flex flex-col bg-section-200 drop-shadow-xl rounded-md py-4 px-5 gap-3">
+      <div className="flex flex-col bg-section-200 drop-shadow-xl rounded-md py-4 px-5 gap-3 mb-4">
         <h3 className="text-white font-semibold uppercase text-base">
           Books you read
         </h3>
-        <div className="text-white flex gap-4 text-base">
+        <div className="text-white flex gap-4 text-base justify-between pr-6 pb-2">
           <div>#️⃣ 2 books</div>
           <div>⭐ 9.20</div>
           <div>🗨️ 2</div>
+          <div>📄 150</div>
         </div>
       </div>
-      {}
+      {books.map((book) => (
+        <SavedBook key={book.id} book={book} />
+      ))}
     </>
   );
 }
