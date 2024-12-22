a27ae9dd8b7797af2ad662347ed26bdcb2d10a80
Jappanjot26 <singhjappanjot1@gmail.com>
2024-12-23 02:37:26 +0530

    feat: login page

---
+++ src/components/ui/Login.jsx
@@ -0,0 +1,77 @@
+import login from "../svg/login.svg";
+
+function Login() {
+  return (
+    <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center py-2 px-5 md:mx-0 md:my-0 bg-background relative">
+      <div className="absolute top-0 left-0 text-white p-4 text-xl">
+        📕BookNXT
+      </div>
+      <div className="md:w-1/3 max-w-sm">
+        <img
+          src={login}
+          alt="Sample image"
+          className="max-md:h-72 max-md:w-72"
+        />
+      </div>
+      <div className="md:w-1/3 max-w-sm">
+        <div className="flex justify-center">
+          <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-200 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-200 dark:hover:text-slate-300 hover:shadow transition duration-150 focus:outline-none focus:ring focus:border-blue-300">
+            <img
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              loading="lazy"
+              alt="google logo"
+            />
+            <span>Login with Google</span>
+          </button>
+        </div>
+        <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+          <p className="mx-4 mb-0 text-center font-semibold text-slate-500">
+            Or
+          </p>
+        </div>
+        <input
+          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded focus:outline-none focus:ring focus:border-blue-300"
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4 focus:outline-none focus:ring focus:border-blue-300"
+          type="password"
+          placeholder="Password"
+        />
+        <div className="mt-4 flex justify-between font-semibold text-sm">
+          {/* <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
+            <input className="mr-1" type="checkbox" />
+            <span>Remember Me</span>
+          </label> */}
+          <a
+            className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+        <div className="text-center md:text-left flex justify-center">
+          <button
+            className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+        <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
+          Don't have an account?{" "}
+          <a
+            className="text-red-600 hover:underline hover:underline-offset-4"
+            href="#"
+          >
+            Register
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
