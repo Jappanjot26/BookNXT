6d3d5e934fc0506350f2b8f3e0d6162a959cfe40
Jappanjot26 <singhjappanjot1@gmail.com>
2024-12-28 00:09:28 +0530

    feat: login and signup functionality through mongoDB

---
+++ backend/Models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const mongoURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongoURL)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => {
+    console.log("MongoDB connection error: ", err);
+  });
